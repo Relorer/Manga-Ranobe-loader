71178fa5570f0684ad045f3cdfb2e47e6a61c37f
Trofimov Iurii <y.tm542@gmail.ru>
2020-08-01 09:49:27 +0300

    Add: insert title

--- src/pdfCreater.py
+++ src/pdfCreater.py
@@ -1,18 +1,25 @@
 from fpdf import FPDF
 from PIL import Image
 
-
-def add_image(image_path, pdfName):
-    pdf = FPDF()
+def add_image(pdf, imagePath):
     pdf.add_page()
-    (width, height) = Image.open(image_path).size
+    (width, height) = Image.open(imagePath).size
     imageHeightOnPage = height * pdf.w / width
     imageWidthOnPage = width * pdf.h / height
-    if imageHeightOnPage > pdf.h: 
+    if imageHeightOnPage > pdf.h:
         x = (pdf.w - imageWidthOnPage) / 2
-        pdf.image(image_path, x=x, y=0, h=pdf.h)
+        pdf.image(imagePath, x=x, y=0, h=pdf.h)
     else:
         y = (pdf.h - imageHeightOnPage) / 2
-        pdf.image(image_path, x=0, y=y, w=pdf.w)
-    pdf.output(pdfName + ".pdf")
+        pdf.image(imagePath, x=0, y=y, w=pdf.w)
 
+
+def add_title(pdf, title):
+    pdf.add_page()
+    tempPDF = FPDF()
+    tempPDF.set_font(pdf.font_family, size=pdf.font_size)
+    tempPDF.add_page()
+    tempPDF.text(0,0,title)
+    x = (pdf.w - pdf.get_string_width(title)) / 2
+    y = (pdf.h - tempPDF.y) / 2
+    pdf.text(x, y, title)
