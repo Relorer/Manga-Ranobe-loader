fbf02e43e3aaae4ae1099b57303250ecc1eb307e
Trofimov Iurii <y.tm542@gmail.ru>
2020-08-07 22:05:11 +0300

    Planning to download

--- src/main.py
+++ src/main.py
@@ -33,31 +33,37 @@ def setProgressForCreation(progress):
 
 
 def main():
+    urls = []
     downloadPath = "download_manga"
     while True:
         print(Style.RESET_ALL, end="")
-        print("Enter a link to the manga: ", end="")
-        url = input()
+        while True:
+            print("Enter a link to the manga: ", end="")
+            url = input()
+            if url == "":
+                break
+            urls.append(url)
         print("Enter the maximum PDF file size (MB): ", end="")
         try:
             sizeLimit = abs((int)(input()) * 1024 * 1024)
         except:
             print(Fore.RED + "The size must be a number")
             continue
-        try:
-            manga = get_manga(url, setProgressForParsing)
-        except:
-            print(Fore.RED + "Invalid link, don't try to fuck up the system")
-            continue
-        try:
-            download_manga(downloadPath, manga, setProgressForLoading)
-        except:
-            print(
-                Fore.BLUE + "Please install the Google Chrome browser in the default folder")
-            continue
-        create_manga_pdf(downloadPath, manga,
-                         sizeLimit, setProgressForCreation)
-        shutil.rmtree(downloadPath)
+        for url in urls:
+            try:
+                manga = get_manga(url, setProgressForParsing)
+            except:
+                print(Fore.RED + "Invalid link, don't try to fuck up the system")
+                continue
+            try:
+                download_manga(downloadPath, manga, setProgressForLoading)
+            except:
+                print(
+                    Fore.BLUE + "Please install the Google Chrome browser in the default folder")
+                continue
+            create_manga_pdf(downloadPath, manga,
+                            sizeLimit, setProgressForCreation)
+            shutil.rmtree(downloadPath)
 
 
 if __name__ == "__main__":
